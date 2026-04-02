378717332a8ff3ad7b01b518a22648db48612ac9
Madhusudan <madhusudan04337@gmail.com>
2026-04-02 18:49:42 +0530

    feat: add Attendance model with compound index and check-in out fields

---
+++ server/models/Attendance.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const { ATTENDANCE_STATUS } = require("../utils/constants")
+
+const attendanceSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true,"User is requried"],
+        },
+        date: {
+            type: Date,
+            requried: [true, "Date is required"],
+        },
+        status: {
+            type: String,
+            enum: Object.values(ATTENDANCE_STATUS),
+            default: ATTENDANCE_STATUS.PRESENT,
+        },
+        checkIn: {
+            type: Date,
+            default: null,
+        },
+        checkOut: {
+            type: Date,
+            default: null,
+        },
+        note: {
+            type: String,
+            trim: true,
+            default:null,
+        },
+        rectifiedBy: {
+            type: mongoose.Schema.Schema.Types.ObjectId,
+            ref: "User",
+            default: null,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+attendanceSchema.index({ user: 1, date: 1}, { unique: true });
+
+module.exports = mongoose.model("Attendance",attendanceSchema);
