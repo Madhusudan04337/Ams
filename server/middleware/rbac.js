e285b9ea4df8c4586f5c5d796659680a04d7f92d
Madhusudan <madhusudan04337@gmail.com>
2026-04-05 21:38:47 +0530

    feat: add RBAC middleware with requireRole function

---
+++ server/middleware/rbac.js
@@ -0,0 +1,17 @@
+const ApiError = require("../utils/ApiError");
+
+const requireRole = (...roles) =>{
+    return (req, res, next) => {
+        if (!req.user){
+            return next(new ApiError(401, "Unauthorized. Please log in."));
+        }
+
+        if (!req.includes(req.user.role)){
+            return next(new ApiError(403, "Forbidden. You do not have permission."));
+        };
+        
+        next();
+    };
+};
+
+module.exports = { requireRole };
